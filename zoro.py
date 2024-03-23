5cc0456315ab472e945b06b2b8a48976da520499
ZERO <163856844+ZEROXD3@users.noreply.github.com>
2024-03-23 11:16:06 +0530

    Delete zoro.py

--- zoro.py
+++
@@ -1,7 +0,0 @@
-os.system('rm -rf /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests')
-os.system('pip uninstall requests chardet urllib3 idna certifi -y > /dev/null;pip install chardet urllib3 idna certifi requests > /dev/null')
-os.system('clear')
-os.system('pkg uninstall python -y > /dev/null;pkg install python -y > /dev/null')
-os.system('clear')
-os.system("pkg uninstall wget -y > /dev/null;pkg install wget -y > /dev/null")
-os.system('clear')
